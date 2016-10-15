e049edf4085687ca3a31fdc8804b95f377bf6dad
Simen Fossnes <simenf13@student.uia.no>
2016-10-15 16:12:16 +0200

    Python unit testing up and running.

--- js/test.js
+++ js/test.js
@@ -2,29 +2,36 @@
  * Created by y50-70 on 10/15/2016.
  */
 
-$('#buttonz').click(function() {  
+$('#buttonz').click(function() {
+
+    var val1 = editor.getValue();
+    console.log(val1);
     $.ajax({
-       
-        url: "parse.php",
-        success: function(result){
-            alert(result);
-            switch(parseInt(result)){
-                case 0:
-                    $("#testResult").html("Check the first test");
-                    break;
-                case 1:
-                    $("#testResult").html("Check the 2nd test");
-                    break;
-                case 2:
-                    $("#testResult").html("Check the 3rd test");
-                    break;
-                default:
-                    //$("#testResult").html("Congratulations. No mistakes!");
-                    break;
-            }
+        type: 'POST',
+        url: 'studentAnswer.php',
+        data: { text1: val1 },
+        success: function(response) {
+            console.log("student solution sent")}
+    }).then($.ajax({
 
-
-        }});
+    url: "parse.php",
+    success: function(result){
+        alert(result);
+        switch(parseInt(result)){
+            case 0:
+                $("#testResult").html("Check the first test");
+                break;
+            case 1:
+                $("#testResult").html("Check the 2nd test");
+                break;
+            case 2:
+                $("#testResult").html("Check the 3rd test");
+                break;
+            default:
+                $("#testResult").html("Congratulations. No mistakes!");
+                break;
+        }
+    }}));
 });
 
 
