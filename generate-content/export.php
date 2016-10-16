7fdec6e4a9b86af216e58a780b836968929131dc
Simen Fossnes <simenf13@student.uia.no>
2016-10-16 21:07:24 +0200

    It now creates json files with the name of the title of the tutorial.

--- generate-content/export.php
+++ generate-content/export.php
@@ -11,3 +11,8 @@ $result = json_encode($_POST);
 $fp = fopen('export.json', 'w+');
 fwrite($fp, $result);
 fclose($fp);
+
+$filename = json_decode($_POST['title']) . ".json";
+$fp = fopen($filename, 'w+');
+fwrite($fp, $result);
+fclose($fp);
