3333967910af9ef31febd585cbe0928ac85a44f7
Ragnar Bjørnson <ragnarbjrnson@gmail.com>
2016-10-16 14:06:02 +0200

    bug fixing

---
+++ Template/Python27/Punit/something.py
@@ -0,0 +1,7 @@
+data = open("../Ampps/www/Ph2/Template/Python27/Punit/MA147C1Q1.txt", "r")
+lines = data.readlines()
+data.close()
+
+my_lst_str = ''.join(map(str, lines)).replace(" ", "").replace("\n", "")
+
+#print my_lst_str
