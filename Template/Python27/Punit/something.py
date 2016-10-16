8e96ec8b50810ded0a2e0daf75fb19c80847023d
magnus <magnus01lysfjord@gmail.com>
2016-10-16 17:08:17 +0200

    ADD YOUR PYTHON PATH IN THE VARIABLE IN FILE Template/Python27/Punit/something.py

--- Template/Python27/Punit/something.py
+++ Template/Python27/Punit/something.py
@@ -1,7 +1,17 @@
-data = open("/../xampp/htdocs/phase2/Template/Python27/Punit/MA147C1Q1.txt", "r")
+c = "Program Files (x86)"
+cmd = "C:/%s/Ampps/www/Ph2/Template/Python27/Punit/MA147C1Q1.txt" % c
+
+#data = open('c:/Python27 MA147C1Q1.txt', "r")
+data = open(cmd, "r")
+#data = open("/../xampp/htdocs/phase2/Template/Python27/Punit/MA147C1Q1.txt", "r")
 lines = data.readlines()
 data.close()
 
 my_lst_str = ''.join(map(str, lines)).replace(" ", "").replace("\n", "")
 
 #print my_lst_str
+
+
+
+
+
