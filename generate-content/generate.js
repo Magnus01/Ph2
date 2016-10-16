9d2a00661004fdd196c6ae9ba1cfcc81f4db0cb8
Simen Fossnes <simenf13@student.uia.no>
2016-10-16 16:23:56 +0200

    Added basic JSON functionality. It now takes information from the page and converts it into values that is possible to store with JSON

--- generate-content/generate.js
+++ generate-content/generate.js
@@ -1,6 +1,25 @@
 function generateContent(){
 
-    // generate content
+    // get content
+    var title = JSON.stringify(document.getElementById("title").innerText);
+    var tutorial = JSON.stringify(document.getElementById("tutorial").innerHTML);
+    var extitle = JSON.stringify(document.getElementById("extitle").innerHTML);
+    var exdesc = JSON.stringify(document.getElementById("exdesc").innerHTML);
 
+    // display the content
+    alert(title);
+    alert(tutorial);
+    alert(extitle);
+    alert(exdesc);
+
+    // generate content
+    var theJson = {
+        "title": title,
+        "tutorial": tutorial,
+        "extitle": extitle,
+        "exdesc": exdesc
+    };
+
+    alert(theJson);
 
 }
