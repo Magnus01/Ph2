a605e21f44011c4bc1d371344b5b4cb72f6822f8
Magnus01 <magnus01lysfjord@gmail.com>
2017-01-26 07:37:27 +0100

    Add files via upload
    Postrue contains login, dbconnect classes, addcourse class

---
+++ gentelella/postrue/classes/model.class.php
@@ -0,0 +1,139 @@
+<?php
+
+    /*
+     The model class will be used to create functions that can help us interact with our database
+    */
+
+    class Model 
+    {
+
+        private $errors = array();
+
+        public function __construct( $dbh )
+        {
+            $this->db = $dbh->db;
+        }
+
+        /**
+        */
+        public function validateForm( $data )
+        {
+
+            // Verify if all fields are filled
+            foreach ($data as $index => $value) {
+                if ( empty($value) || trim($value) == '' ) 
+                {
+                    $this->errors['error_'.$index] =  'Please fill the '.$index.' field.';
+                }
+            }
+            // We return false if there is no error, otherwise we return the errors
+            return sizeof($this->errors) == 0 ? false : $this->errors;
+
+        }
+
+
+        public function hash_password( $password )
+        {
+            if (!empty($password)) {
+                $jeton = "";
+                $salt_chars = array_merge(range('A','Z'), range('a','z'), range(0,9));
+                for($i = 0; $i < 22; $i++) {
+                  $jeton .= $salt_chars[array_rand($salt_chars)];
+                }
+                return crypt($password, sprintf('$2y$%02d$', 7) . $jeton);
+
+            }else{
+                return false;
+            }
+        }
+        
+        public function getId($email) 
+        {
+            $query = $this->db->prepare( " SELECT id 
+                                        FROM User
+                                        WHERE email = ?
+                                    " );
+              if ($query->execute(array($email)))
+            {
+                
+                $query->setFetchMode(PDO::FETCH_OBJ);
+                $fetchedData = $query->fetch();
+                return $fetchedData->id;
+            
+            
+        }}
+
+
+        public function signIn( $email, $password )
+        {
+            $query = $this->db->prepare( " SELECT email, password, id 
+                                        FROM User
+                                        WHERE email = ?
+                                    " );
+            
+         
+            if ($query->execute(array($email)))
+            {
+                
+                $query->setFetchMode(PDO::FETCH_OBJ);
+                $fetchedData = $query->fetch();
+              
+                
+//                console_log($fetchedData->id . "fetchedData");
+//                    
+                
+                $digest = isset($fetchedData->password) ? $fetchedData->password : null;
+                if (!is_null($digest)) {
+//                    return $digest === crypt($password);
+                    return $digest === crypt($password, $digest);
+                   
+                }else{
+                    return false;
+
+                }
+            }else{
+                return false;
+            
+            }
+
+        }
+        
+         
+        
+
+        public function emailAlreadyUsed( $email )
+        {
+            $query = $this->db->prepare("SELECT COUNT(*) AS num FROM User WHERE email = ?");
+            if ($query->execute(array($email)))
+            {
+                $query->setFetchMode(PDO::FETCH_OBJ);
+                $fetchedData = $query->fetchAll();
+
+                $total = isset($fetchedData[0]) ? $fetchedData[0]->num : null;
+                return $total != 0;
+            }else{
+                return false;
+            }
+        }
+
+
+        public function addUser($email, $password, $type, $name, $surname)
+        {
+            if (!empty($email) && !empty($password)) {
+                $query = $this->db->prepare("INSERT INTO User (email, password, type, name, surname)  VALUES(?,?,?,?,?) ");
+                return $query->execute(array($email, $this->hash_password($password), $type, $name, $surname));
+            }else{
+                return false;
+            }
+        }
+        
+         public function addCourse($id, $title, $description, $User_id)
+        {
+          
+                $query = $this->db->prepare("INSERT IGNORE INTO Course (id, title, description, creation_timestamp, User_id )  VALUES(?,?,?,?,?) ");
+                return $query->execute(array($id, $title, $description, time(), $User_id));
+           
+                
+        }
+        
+    }
---
+++ gentelella/postrue/includes2/log-user-in.php
@@ -0,0 +1,49 @@
+<?php
+require __DIR__. '/../config2/dbconnect.php'; // database connection
+require __DIR__. '/../classes/model.class.php'; // Model
+
+
+$Dbhandler  = new Config() ;
+$Model      = new Model($Dbhandler);
+
+session::init();
+
+if ( isset($_POST) && !empty($_POST) ) 
+{
+    
+    if ( !$return = $Model->validateForm($_POST)  ) 
+    {
+        $email      =  filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+        $password   =  $_POST['password'];
+
+        
+        console_log($password . " " . "password");
+       $id = $Model->getId($email);
+            console_log($id . "id??");
+            
+    
+
+        if ($Model->signIn($email, $password, $id)) 
+        {   
+               
+            
+          
+            session::set('user_session', $id);
+            
+              isset($_SESSION['user_session']) ? header("location:dashboard2.php"):null;
+        }else{
+            session::set('login_issue', "Can't log you in. check your details.");
+        }
+
+    }else{
+        foreach ($return as $field => $error) {
+            session::set($field, $error);
+        }
+    }
+
+
+}
+
+
+
+
