be46f20c48b110907cae9c120f636cef3b6a3a34
magnus <magnus01lysfjord@gmail.com>
2017-01-27 13:56:00 +0100

    execute.js

---
+++ generate-content/execute.js
@@ -0,0 +1,35 @@
+function execUnitTest(){
+
+    processHints();
+    
+    
+    
+    
+$('#buttonz').click(function() {
+ 
+    var val1 = editor.getValue();
+    console.log(val1);
+    $.ajax({
+        type: 'POST',
+        url: 'studentAnswer.php',
+        data: { text1: val1 },
+        success: function(response) {
+            console.log("student solution sent")}
+    }).then($.ajax({
+
+    url: "parse.php",
+    success: function(result){
+            // if it's minus one, we have the right answer, if not; display the correct hint
+            if (response == "-1") {
+                $('#testResult').html('<blockquote id="hint" style="background: #33cc33; color:white; border-left: 2px solid #33aa33"><i>Hint:</i><br/>'
+                    + 'Your answer was correct! Congratulations!!!' + '</blockquote>');
+            } else {
+                $('#testResult').html('<blockquote id="hint" style="background: #c33; color:white;"><i>Hint:</i><br/>'
+                    + hints[parseInt(response)+1] + '</blockquote>');
+            }
+
+
+        }
+    })
+
+)};
