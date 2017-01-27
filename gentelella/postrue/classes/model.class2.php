0c020fd4727979501509979f31c37030b8424bb2
magnus <magnus01lysfjord@gmail.com>
2017-01-27 07:40:30 +0100

    Edits created in model.class2 in order to link course to exercise page

--- gentelella/postrue/classes/model.class2.php
+++ gentelella/postrue/classes/model.class2.php
@@ -46,50 +46,64 @@
                 return false;
             }
         }
+        
+        public function getId($email) 
+        {
+            $query = $this->db->prepare( " SELECT id 
+                                        FROM User
+                                        WHERE email = ?
+                                    " );
+              if ($query->execute(array($email)))
+            {
+                
+                $query->setFetchMode(PDO::FETCH_OBJ);
+                $fetchedData = $query->fetch();
+                return $fetchedData->id;
+            
+            
+        }}
 
 
         public function signIn( $email, $password )
         {
-            $query = $this->db->prepare( " SELECT email, password 
-                                        FROM User 
+            $query = $this->db->prepare( " SELECT email, password, id 
+                                        FROM User
                                         WHERE email = ?
                                     " );
+            
+         
             if ($query->execute(array($email)))
             {
+                
                 $query->setFetchMode(PDO::FETCH_OBJ);
                 $fetchedData = $query->fetch();
-                console_log(isset($fetchedData->password));
+              
+                
+//                console_log($fetchedData->id . "fetchedData");
+//                    
+                
                 $digest = isset($fetchedData->password) ? $fetchedData->password : null;
                 if (!is_null($digest)) {
-                    
-                    console_log($password . " " . " PASSWORD REAL");
-                    console_log($digest . " " . " DIGEST");
-                    
-                    console_log(crypt($password, $digest) . " " . "PASSWORD digest and crypt");
-                    console_log(crypt($digest) . " " . "digest");
-                    console_log(crypt($password) . " " . "PASSWORD ");
-                    console_log(crypt(1) . " " . "1");
-                    console_log(crypt("1", $digest) . " " . "1 string and digest");
-                    console_log(crypt(1, $digest) . " ". "1 and digest");
-//                    return $digest === $password;
-                    
-//                    console_log($digest);
-//                    console_log(crypt($password, $digest));
+//                    return $digest === crypt($password);
                     return $digest === crypt($password, $digest);
-//                   
-                    
+                   
                 }else{
                     return false;
+
                 }
             }else{
                 return false;
+            
             }
 
         }
+        
+         
+        
 
         public function emailAlreadyUsed( $email )
         {
-            $query = $this->db->prepare("SELECT COUNT(*) AS num FROM users WHERE user_email = ?");
+            $query = $this->db->prepare("SELECT COUNT(*) AS num FROM User WHERE email = ?");
             if ($query->execute(array($email)))
             {
                 $query->setFetchMode(PDO::FETCH_OBJ);
@@ -103,15 +117,142 @@
         }
 
 
-        
-      
-        public function addUser($email, $password)
+        public function addUser($email, $password, $type, $name, $surname)
         {
             if (!empty($email) && !empty($password)) {
-                $query = $this->db->prepare("INSERT INTO users (user_email, user_password, date_signed_up)  VALUES(?,?,?) ");
-                return $query->execute(array($email, $this->hash_password($password), time()));
+                $query = $this->db->prepare("INSERT INTO User (email, password, type, name, surname)  VALUES(?,?,?,?,?) ");
+                return $query->execute(array($email, $this->hash_password($password), $type, $name, $surname));
             }else{
                 return false;
             }
         }
+        
+         public function addCourse($id, $title, $description, $User_id)
+        {
+          
+                $query = $this->db->prepare("INSERT IGNORE INTO Course (id, title, description, creation_timestamp, User_id )  VALUES(?,?,?,?,?) ");
+                return $query->execute(array($id, $title, $description, time(), $User_id));
+             {
+
+                $query->setFetchMode(PDO::FETCH_OBJ);
+                $fetchedData = $query->fetch();
+                return $fetchedData->id;
+
+
+            }
+           
+                
+        }
+
+
+        public function getExerciseOrder($CourseId)
+        {
+            $query = $this->db->prepare( " SELECT COUNT(`order`) FROM Exercise WHERE course_id = ?" );
+            if ($query->execute(array($CourseId)))
+            {
+
+                $query->setFetchMode(PDO::FETCH_OBJ);
+                $fetchedData = $query->fetch();
+                return $fetchedData->id;
+
+
+            }
+        }
+
+        public function getExercises($Course_id) {
+            $query = $this->db->prepare( "SELECT * FROM `Exercise` WHERE `Course_id`=?" );
+            if ($query->execute(array($Course_id)))
+            {
+                $exercises = array();
+
+                $query->setFetchMode(PDO::FETCH_OBJ);
+                while($row = $query->fetch()) {
+                    array_push($exercises, $row);
+                    //var_dump($row);
+                }
+
+                return $exercises;
+
+
+            }
+        } 
+        
+        public function getCourses($User_id) {
+            $query = $this->db->prepare( "SELECT * FROM `Course` WHERE `User_id`=?" );
+            if ($query->execute(array($User_id)))
+            {
+                $courses = array();
+
+                $query->setFetchMode(PDO::FETCH_OBJ);
+                while($row = $query->fetch()) {
+                    array_push($courses, $row);
+                    //var_dump($row);
+                }
+
+                return $courses;
+
+
+            }
+        }
+
+        public function getCourse($Course_id) {
+            $query = $this->db->prepare( "SELECT * FROM `Course` WHERE `id`=?" );
+            if ($query->execute(array($Course_id)))
+            {
+
+                $query->setFetchMode(PDO::FETCH_OBJ);
+                $result = $query->fetch();
+
+                return $result;
+
+            }
+        }
+
+        public function addExercise($title, $content, $tasktitle, $taskdescription, $level, $User_id, $Course_id)
+        {
+
+            /* get other variables
+            $sql = $this->db->prepare("SELECT COUNT(`order`) FROM Exercise;");
+            $order = $sql->execute();
+            */
+
+            $query = $this->db->prepare("INSERT IGNORE INTO Exercise (`order`, `title`, `content`, `task title`, `task description`, `level`, `creation_timestamp`, `User_id`, `Course_id`) VALUES (?,?,?,?,?,?,?,?,?)");
+            return $query->execute(array(1, $title, $content, $tasktitle, $taskdescription, $level, time(), $User_id, $Course_id));
+
+        }
+        
+    
+
+    public function CourseId2($id) {
+           $query = $this->db->prepare( "SELECT id FROM `Course` WHERE `id`=?" );
+       
+            $query ->execute(array($id));
+            $result = $query->fetchAll();
+            return $result;
+    }
+        
+        
+        
+    public function CourseId($id) {
+           $query = $this->db->prepare( "SELECT Course.id FROM Exercise JOIN Course On Exercise.Course_id = Course.id WHERE Course.id = ? " );
+       
+            $query ->execute(array($id));
+            $result = $query->fetchColumn();;
+            return $result;
+    }
+        
+
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
