f01f1b7b52ea002c02478b9dc7e0287de7bdce00
Simen Fossnes <simen.fossnes@gmail.com>
2017-02-02 17:12:32 +0100

    No more editable content.

--- generate-content/text.py
+++ generate-content/text.py
@@ -2,8 +2,5 @@ from something import string
 import unittest
 class TestStringMethods(unittest.TestCase):
 
-	def test_ca(self):
-	    self.assertIn("return", string)
-	
 if __name__ == '__main__':
 	unittest.main()
